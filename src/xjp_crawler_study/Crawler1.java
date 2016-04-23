905afff7b1666615084a19d970cbbab3db6448d3
xjp <xjpsjtu@sjtu.edu.cn>
2016-04-23 10:58:38 +0800

    modified@file

--- src/xjp_crawler_study/Crawler1.java
+++ src/xjp_crawler_study/Crawler1.java
@@ -7,6 +7,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Crawler1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String url = "https://www.zhihu.com/explore/recommendations";
+		// String url = "http://www.zhihu.com/question/43534800";
+		String result = SendGet(url);
+		// System.out.println(result);
+		// ArrayList<String> ContSrc = RegexString(result,
+		// "<h2.*?>\n+(.+?)\n+</h2>");
+		ArrayList<Zhihu> ContSrc = GetZhihuUrl(result);
+		for(Zhihu zhihu : ContSrc){
+			FileReaderWriter.writeIntoFile(zhihu.writeString(), "E:/zhihu.txt", true);
+		}
+//		for (int i = 0; i < ContSrc.size(); i++) {
+//			ContSrc.get(i).print();
+//		}
+
+	}
+
 	static String SendGet(String url) {
 		// 定义一个字符串来存储网页内容
 		String result = "";
@@ -42,27 +60,27 @@ public class Crawler1 {
 		return result;
 	}
 
-	static ArrayList RegexString(String targetStr, String patternStr){
-		//定义一个样式模板，此种使用正则表达式，括号中是要抓取的内容
+	static ArrayList RegexString(String targetStr, String patternStr) {
+		// 定义一个样式模板，此种使用正则表达式，括号中是要抓取的内容
 		ArrayList<String> reslist = new ArrayList<String>();
 		Pattern pattern = Pattern.compile(patternStr);
 		Matcher matcher = pattern.matcher(targetStr);
 		boolean isFind = matcher.find();
-		while(isFind){
+		while (isFind) {
 			reslist.add(matcher.group(1));
 			isFind = matcher.find();
 		}
 		return reslist;
 	}
-	
-	static ArrayList<Zhihu> GetZhihu(String content){
+
+	static ArrayList<Zhihu> GetZhihu(String content) {
 		ArrayList<Zhihu> reslist = new ArrayList<Zhihu>();
 		Pattern questionPattern = Pattern.compile("question_link.+?>(.+?)<");
 		Matcher questionMatcher = questionPattern.matcher(content);
 		Pattern urlPattern = Pattern.compile("question_link.+?href=\"(.+?)\"");
 		Matcher urlMatcher = urlPattern.matcher(content);
 		boolean isFind = questionMatcher.find() && urlMatcher.find();
-		while(isFind){
+		while (isFind) {
 			Zhihu zhihu = new Zhihu();
 			zhihu.question = questionMatcher.group(1);
 			zhihu.zhihuUrl = "http://www.zhihu.com" + urlMatcher.group(1);
@@ -71,31 +89,20 @@ public class Crawler1 {
 		}
 		return reslist;
 	}
-	static ArrayList<Zhihu> GetZhihuUrl(String content){
+
+	static ArrayList<Zhihu> GetZhihuUrl(String content) {
 		ArrayList<Zhihu> reslist = new ArrayList<Zhihu>();
 		Pattern urlPattern = Pattern.compile("<h2>.+?question_link.+?href=\"(.+?)\".+?</h2>");
 		Matcher urlMatcher = urlPattern.matcher(content);
 		boolean isFind = urlMatcher.find();
-		while(isFind){
-//			System.out.println("This is the urlMatcher: " + urlMatcher.group(1));
+		while (isFind) {
+			// System.out.println("This is the urlMatcher: " +
+			// urlMatcher.group(1));
 			Zhihu zhihu = new Zhihu(urlMatcher.group(1));
 			reslist.add(zhihu);
 			isFind = urlMatcher.find();
 		}
 		return reslist;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String url = "https://www.zhihu.com/explore/recommendations";
-//		String url = "http://www.zhihu.com/question/43534800";
-		String result = SendGet(url);
-//		System.out.println(result);
-//		ArrayList<String> ContSrc = RegexString(result, "<h2.*?>\n+(.+?)\n+</h2>");
-		ArrayList<Zhihu> ContSrc = GetZhihuUrl(result);
-		for(int i = 0; i < ContSrc.size(); i++){
-			ContSrc.get(i).print();
-		}
-
 	}
 
 }
